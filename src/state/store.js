eaa0581ef691a7f42eb6186c8de42a8130924d7a
richierob62 <richierob62@gmail.com>
2019-08-19 20:25:58 -0400

    Refactor Store Creation

--- src/state/store.js
+++ src/state/store.js
@@ -2,8 +2,19 @@ import AppleState from '../state/apple_state'
 import PearState from '../state/pear_state'
 import React from 'react'
 
-export default ({ children }) => (
-  <PearState>
-    <AppleState>{children}</AppleState>
-  </PearState>
-)
+// add states here
+const allStates = [AppleState, PearState]
+
+export default ({ children }) => {
+  let output = null
+
+  allStates.forEach(State => {
+    if (!output) {
+      output = <State>{children}</State>
+    } else {
+      output = <State>{output}</State>
+    }
+  })
+
+  return output
+}
